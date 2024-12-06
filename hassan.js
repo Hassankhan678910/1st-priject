eff88652a12ebc8e17bf339f59b0feb09ecfc8b9
Hassankhan678910 <hassssankhan2007@gmail.com>
2024-12-06 02:42:43 -0800

    Add files via upload

---
+++ hassan.js
@@ -0,0 +1,5 @@
+$("#hh").click(function(){
+
+$("p").css("color","blue").toggle(1000);
+
+})                                                                                                                                  
